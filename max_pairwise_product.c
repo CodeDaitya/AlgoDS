7b86819090203db845cb1af8deb5e983e49d6d8b
Deepak Maurya <itsdpm@gmail.com>
2017-03-19 20:16:38 +0530

    Max pairwise product, stress tested code

--- max_pairwise_product.c
+++ max_pairwise_product.c
@@ -1,26 +1,81 @@
 #include<stdio.h>
+#include<malloc.h>
+#include<stdlib.h>
+
+int *numbers;
+
+long long max_pairwise_product(int );
+long long max_pairwise_product_fast(int );
 
 int main()
 {
   int n;
-  scanf("%d", &n);
 
-  int *numbers;
-  int max_pairwise_prod=0;
+  long long result1, result2;
 
-  for(int i = 0;i<n;++i){
+  /*for(int i = 0;i<n;++i){
     scanf("%d", numbers+i);
+  }*/
+
+  while(1){
+    srand(time(NULL));
+    n = rand()%10 + 2;
+    numbers = (int*)malloc(sizeof(int)*n);
+
+    printf("%d\n", n);
+
+    for(int i = 0;i < n; i++){
+      *(numbers + i) = rand()%100000;
+    }
+    for(int i = 0;i < n; i++){
+      printf("%d ", *(numbers + i));
+    }
+    printf("\n");
+
+    result1 = max_pairwise_product(n);
+    result2 = max_pairwise_product_fast(n);
+
+    if(result1!=result2){
+      printf("Warning: %lld %lld\n", result1, result2);
+      break;
+    }
+    else
+      printf("OK");
   }
 
-  for(int i=0;i<n;++i){
-    for(int j=0;j<n;++j){
+  return 0;
+}
+
+long long max_pairwise_product(int n){
+  long long result = 0;
+
+  for(int i = 0;i<n;++i){
+    for(int j;j<n;++j){
       if(i==j)continue;
 
-      if(*(numbers+i)*(*(numbers+j))>max_pairwise_prod)
-        max_pairwise_prod = *(numbers+i)*(*(numbers+j));
+      if((long long)(*(numbers + i))*(*(numbers + j))>result)
+        result = (long long)(*(numbers + i))*(*(numbers + j));
     }
-	}
-  printf("%d\n", max_pairwise_prod);
-  
-  return 0;
+  }
+
+  return result;
+}
+
+long long max_pairwise_product_fast(int n){
+  long long result;
+
+  int* num = numbers;
+  int max_index1 = -1;
+  for(int i = 0;i<n;++i){
+   if(max_index1 == -1||(*numbers>*max_index1))
+    max_index1 = i;
+  }
+
+  int max_index2 = -1;
+  for(int i = 0;i<n;++i){
+    if(max_index2!=max_index2&&(max_index2 == -1||*numbers>(*(numbers+max_index2))))
+      max_index2 = i;
+  }
+
+  return (long long)*(numbers+max_index1)*(*(numbers+max_index2));
 }
